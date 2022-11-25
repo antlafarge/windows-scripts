9a36b73e1e61aaa7660edef3ea289d736b37dcdb
Antoine Lafarge <ant.lafarge@gmail.com>
2022-11-25 15:42:04 +0100

    Update zipbruteforce.ps1

--- zipbruteforce.ps1
+++ zipbruteforce.ps1
@@ -1,6 +1,9 @@
-$passwords = Get-Content ".\10000passwords.txt"
+# Usage : .\zipbruteforce.ps1 ".\archive.zip" ".\passwords.txt"
+
+$zipFile = $args[0]
+$passwordsFile = $args[1]
+$passwords = Get-Content $passwordsFile
 $7ZipPath = "C:\Program Files\7-Zip\7z.exe"
-$zipFile = "zipFile.zip"
 foreach ($password in $passwords)
 {
     & $7ZipPath "t" $zipFile "-p$password" > $null
