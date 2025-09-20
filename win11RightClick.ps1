0db5cdf3e5c9af9b01a1487d2f28241b60a69598
Antoine Lafarge <ant.lafarge@gmail.com>
2025-09-20 13:03:46 +0200

    Update and rename win11RightClick.reg to win11RightClick.ps1

---
+++ win11RightClick.ps1
@@ -0,0 +1,5 @@
+; Disable new right click menu
+reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+
+; Enable new right click menu
+reg delete "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" /f
